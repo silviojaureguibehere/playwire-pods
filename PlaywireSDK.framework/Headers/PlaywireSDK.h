21a0ecf9f5b229f2120a011e4b4d53feb7f56dff
Silvio Jaureguibehere <silvio.jaureguibehere@gmail.com>
2016-11-28 09:57:00 -0300

    Version 0.1.0

---
+++ PlaywireSDK.framework/Headers/PlaywireSDK.h
@@ -0,0 +1,19 @@
+//
+//  PlaywireSDK.h
+//  PlaywireSDK
+//
+//  Created by Silvio Jaureguibehere on 4/22/16.
+//  Copyright © 2016 Playwire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlaywireSDK.
+FOUNDATION_EXPORT double PlaywireSDKVersionNumber;
+
+//! Project version string for PlaywireSDK.
+FOUNDATION_EXPORT const unsigned char PlaywireSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaywireSDK/PublicHeader.h>
+
+
